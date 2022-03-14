3d0761e2d8acc0fa930edf5e5bc29755450f9224
akchabekova <Luelum7@gmail.com>
2022-03-14 16:20:35 +0100

    finished

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-const title = document.querySelector('.title')
-const descRandom = document.querySelector('.desc-random')
+let descRandom = document.querySelector('.desc-random')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.input')
+let guess = 3
+descRandom = Math.floor(Math.random() * 10)
+
+
+
+btn.addEventListener('click',()=>{
+    if (input.value === descRandom ) {
+        alert("Вы победили!")
+    } else if( guess > 1) {
+       alert( descRandom.textContent=`Попробуйте еще раз, у вас осталось ${guess = guess -1 }  попытки`)
+    } else {
+        alert('Игра окончена, Вы проиграли!')
+    }
+}
+)
+
+
